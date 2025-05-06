dd6dd91d35e74a1b6fbeda30fc08d8706c0cdcaf
Theib9ja <114008979+Theib9ja@users.noreply.github.com>
2025-05-06 13:27:25 -0700

    made changes to the javascript

--- src/javascript/index.js
+++ src/javascript/index.js
@@ -1,7 +1,11 @@
 const allButton = document.querySelector('.all-btn')
 
+const podcastButton = document.querySelector('.podcast-btn')
+
 allButton.addEventListener('click', () => {
     allButton.classList.add('color-click')
+    podcastButton.classList.remove('color-click')
+    musicButton.classList.remove('color-click')
     
 })
 
@@ -11,6 +15,14 @@ musicButton.addEventListener('click', musicClick)
     
     function musicClick() {
     musicButton.classList.add("color-click")
+    allButton.classList.remove('all-btn')
     allButton.classList.remove('color-click')
+    podcastButton.classList.remove('color-click')
 
 }
+
+podcastButton.addEventListener('click', () => {
+    podcastButton.classList.add('color-click')
+    allButton.classList.remove('color-click')
+    musicButton.classList.remove('color-click')
+})
